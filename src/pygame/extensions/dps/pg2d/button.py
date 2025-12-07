ed27faf2a11d856b681cf1089105443e13c8f9b0
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-12-06 20:33:44 -0500

    Revert text to ButtonOptions param

--- src/pygame/extensions/dps/pg2d/button.py
+++ src/pygame/extensions/dps/pg2d/button.py
@@ -14,12 +14,12 @@ class TextOptions(pgcore.Configurable):
     antialias: bool
     color: types.ColorValue
     hover_color: types.ColorValue
-    text: str = ""
 
 
 @dataclasses.dataclass
 class ButtonOptions(sprite.SpriteOptions):
     color: types.ColorValue = ""
+    text: str = ""
     text_opts: TextOptions | None = None
 
 
@@ -36,17 +36,19 @@ class Button(sprite.GameSprite):
     def __init__(self, opts: ButtonOptions, on_click: Callable):
         super().__init__(opts)
         self.color = opts.color
+        self.text = opts.text
         self.text_opts = opts.text_opts
         self.on_click = on_click
 
     def update(self):
-        if self.text_opts is not None:
+        if self.text and self.text_opts is not None:
             text_opts = self.text_opts
 
             text_color = text_opts.hover_color if self.hovered else text_opts.color
-            img = text_opts.font.render(text_opts.text, text_opts.antialias, text_color)
-            w, h = img.get_size()
-            pos = (self.rect.centerx - (w / 2), self.rect.centery - (h / 2))
+            img = text_opts.font.render(self.text, text_opts.antialias, text_color)
+            button_w, button_h = self.rect.size
+            text_w, text_h = img.get_size()
+            pos = ((button_w / 2) - (text_w / 2), (button_h / 2) - (text_h / 2))
 
             self.image.fill(self.color)
             self.image.blit(img, pos)
