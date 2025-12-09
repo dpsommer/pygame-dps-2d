c04e52e06bfcd69457096f47cfa05ba212abdabc
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-12-09 15:45:57 -0500

    Overhaul core 2D physics to fix multiple issues

--- src/pygame/extensions/dps/pg2d/physics.py
+++ src/pygame/extensions/dps/pg2d/physics.py
@@ -1,6 +1,6 @@
 import dataclasses
 import math
-from typing import List, Type
+from typing import List, Tuple, Type
 
 import pygame.extensions.dps.core as pgcore
 
@@ -18,10 +18,10 @@ class PhysicsSettings(pgcore.Configurable):
 
 @dataclasses.dataclass
 class CollisionBox(pgcore.Configurable):
-    top: bool
-    left: bool
-    right: bool
-    bottom: bool
+    top: bool = True
+    left: bool = True
+    right: bool = True
+    bottom: bool = True
     # TODO: collision offsets
 
 
@@ -49,51 +49,53 @@ class PhysicsObject(pgcore.Loadable, common.GameObject):
 
     def __init__(self, settings: PhysicsObjectSettings, rect: pygame.Rect):
         super().__init__(rect)
-
         self.velocity = pygame.Vector2()
         self.collision_box = settings.collision_box
         self.is_static = settings.is_static
         self.last_pos: types.Coordinate = self.rect.topleft
-
-    def falling(self):
-        return self.velocity.y > 0
 
     def apply_force(self, force: pygame.Vector2):
         if not self.is_static:
             self.velocity += force
 
     def impact(self, o: "PhysicsObject"):
-        self.fix_overlap(o)
         o.apply_force(self.velocity)
 
-    def fix_overlap(self, o: "PhysicsObject"):
-        """Move an overlapping object such that it no longer intersects.
+    def towards(self, v: pygame.Vector2, pos: types.Coordinate) -> Tuple[bool, bool]:
+        px, py = pos
+        dx = px - self.rect.centerx
+        dy = py - self.rect.centery
+        return dx * v.x > 0, dy * v.y > 0
 
-        Magnitude of the vector is determined using Pythagorean theorem with
-        the width and height of the clipping area, and vector direction is
-        the inverted direction of impact.
-
-        Args:
-            o (PhysicsObject): the overlapping physics object
-        """
-        overlap = o.rect.clip(self.rect)
-        magnitude = overlap.w**2 + overlap.h**2
-        fix_vector = o.velocity.rotate(180).normalize() * magnitude
-        o.rect.move_ip(fix_vector)
+    def moving_towards(
+        self, p: "PhysicsObject | types.Coordinate"
+    ) -> Tuple[bool, bool]:
+        # XXX: using the object's center will fail for e.g. slopes;
+        # need to refactor this at some point
+        if isinstance(p, PhysicsObject):
+            p = p.rect.center
+        return self.towards(self.velocity, p)
 
     def colliding(self, o: "PhysicsObject") -> bool:
         curr_x, curr_y = self.rect.topleft
         last_x, last_y = self.last_pos
 
+        colliding = False
+
         if self.collision_box.top and o.collision_box.bottom:
-            return last_y > o.rect.bottom >= curr_y
+            colliding = colliding or last_y >= o.rect.bottom >= curr_y
         if self.collision_box.bottom and o.collision_box.top:
-            return last_y + self.rect.h < o.rect.top <= curr_y + self.rect.h
+            colliding = (
+                colliding or last_y + self.rect.h <= o.rect.top <= curr_y + self.rect.h
+            )
         if self.collision_box.left and o.collision_box.right:
-            return last_x > o.rect.right >= curr_x
+            colliding = colliding or last_x >= o.rect.right >= curr_x
         if self.collision_box.right and o.collision_box.left:
-            return last_x + self.rect.w < o.rect.left <= curr_x + self.rect.w
-        return False
+            colliding = (
+                colliding or last_x + self.rect.w <= o.rect.left <= curr_x + self.rect.w
+            )
+
+        return colliding
 
     def reset(self):
         super().reset()
@@ -101,6 +103,7 @@ class PhysicsObject(pgcore.Loadable, common.GameObject):
         self.last_pos = self.origin
 
 
+# TODO: rework the surface concept to have multiple surfaces per object
 class PhysicsSurface(PhysicsObject):
 
     settings_type: Type[PhysicsObjectSettings] = PhysicsSurfaceSettings
@@ -111,26 +114,49 @@ class PhysicsSurface(PhysicsObject):
         self.friction_coefficient = settings.friction_coefficient
         self.restitution_coefficient = settings.restitution_coefficient
 
-    def normal(self, gravity: float) -> pygame.Vector2:
-        magnitude = -gravity * math.cos(math.radians(self.incline))
-        return pygame.Vector2(0, magnitude).rotate(self.incline)
+    def normal(self, o: PhysicsObject) -> pygame.Vector2:
+        tx, ty = o.moving_towards(self)
+        # perpendicular to the surface
+        # FIXME: need this to work for a box; that is, side and top of the same object
+        # maybe surface should be reworked so that an object has multiple surfaces with
+        # different incline values?
+        mx = -o.velocity.x * math.cos(math.radians(90 - self.incline)) if tx else 0
+        my = -o.velocity.y * math.cos(math.radians(self.incline)) if ty else 0
+        return pygame.Vector2(mx, my)
 
     def friction(self, net_force: pygame.Vector2) -> pygame.Vector2:
-        # friction should be a vector opposite the net force
-        friction = net_force.rotate(180).normalize() * self.friction_coefficient
+        if net_force.length() == 0:
+            return pygame.Vector2()
+        mx = self.friction_coefficient * math.cos(math.radians(self.incline))
+        my = self.friction_coefficient * math.cos(math.radians(90 - self.incline))
+        friction_f = pygame.Vector2(mx, my)
+        # parallel to the surface, opposite the direction of force
+        friction_f = friction_f.elementwise() * net_force.rotate(180).normalize()
         # the force of friction cannot exceed the net force on the object
-        return pygame.Vector2(
-            min(net_force.x, friction.x), min(net_force.y, friction.y)
-        )
+        abs_x, abs_y = abs(net_force.x), abs(net_force.y)
+        friction_f.x = pygame.math.clamp(friction_f.x, -abs_x, abs_x)
+        friction_f.y = pygame.math.clamp(friction_f.y, -abs_y, abs_y)
+        return friction_f
+
+    def fix_overlap(self, o: PhysicsObject):
+        """Move an overlapping object such that it no longer intersects.
+
+        Args:
+            o (PhysicsObject): the overlapping physics object
+        """
+        overlap = o.rect.clip(self.rect)
+        normal = self.normal(o)
+        x = pygame.math.clamp(normal.x, -overlap.w, overlap.w)
+        y = pygame.math.clamp(normal.y, -overlap.h, overlap.h)
+        o.rect.move_ip((x, y))
 
     def impact(self, o: PhysicsObject):
-        super().impact(o)
+        if not o.is_static:
+            self.fix_overlap(o)
         # collision with a surface is a special case - apply the normal vector
         # and force of friction based on the object's direction of movement
-        normal = self.normal(o.velocity.y)
-        normal *= 1 + self.restitution_coefficient
-        # FIXME: make sure that this always zeros out
-        o.apply_force(normal)
+        normal = self.normal(o)
+        o.apply_force(normal * (1 + self.restitution_coefficient))
         o.apply_force(self.friction(o.velocity))
 
 
@@ -159,11 +185,21 @@ class PhysicsController(pgcore.Loadable):
     def update(self, dt: float):
         for i, o in enumerate(self._objects):
             o.update(dt)
+
             o.apply_force(self._gravity_f)
 
+            # use a rect 1 pixel larger than o in all directions so that we
+            # still collide when left/right or top/bottom edges are equal
+            boundary_lefttop = (o.rect.left - 1, o.rect.top - 1)
+            boundary_size = (o.rect.w + 2, o.rect.h + 2)
+            boundary = pygame.Rect(boundary_lefttop, boundary_size)
+
             remaining = self._objects[i + 1 :]
-            collisions = o.rect.collideobjectsall(remaining, key=lambda o: o.rect)
+            collisions = boundary.collideobjectsall(remaining, key=lambda o: o.rect)
+
             for c in collisions:
+                if o.is_static and c.is_static:
+                    continue
                 if o.colliding(c):
                     o.impact(c)
                     c.impact(o)
@@ -171,7 +207,10 @@ class PhysicsController(pgcore.Loadable):
             o.velocity.y = min(o.velocity.y, self.terminal_velocity)
             # store last position for collision detection
             o.last_pos = o.rect.topleft
-            o.rect.move_ip(o.velocity * dt)
+            # XXX: is there a better way to apply dt? slight variations won't
+            # have any impact at this scale; maybe better to scale up values
+            # and apply to all changes by passing to apply force..?
+            o.rect.move_ip(o.velocity * (1 + dt))
 
     def reset(self):
         for o in self._objects:
